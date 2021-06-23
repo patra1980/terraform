96c240a73b5b1c6809a05b6df947a2b6028f1bbf
UtpalPatra <77105975+patra1980@users.noreply.github.com>
2021-06-23 23:43:39 +0530

    Delete ec2.tf

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0aeeebd8d2ab47354"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ibm-pub.id
-  key_name      = "vir"
-  tags = {
-    Name = "TF Instance"
-  }
-}
